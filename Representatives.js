bdd12251039d458f74107dbf76f0b3d82ac116d9
Neeva <neeva.n.y@gmail.com>
2020-11-23 12:33:05 -0500

    represent blank

--- Representatives.js
+++ Representatives.js
@@ -16,6 +16,7 @@ async function getRepresentatives() {
       });
       if(userData === null) {
           let firstTime = true;
+          console.log("newUser");
           renderRepresentatives(result.data.officials);
       } else {
           renderRepsAndNotes(result.data.officials, userData);
