3e9c6c098dd1e2c7d239627ae0218dc0febc580a
henrycox <72895874+henrycox@users.noreply.github.com>
2020-11-23 19:50:43 -0500

    fix

--- Representatives.js
+++ Representatives.js
@@ -96,7 +96,7 @@ async function renderRepresentatives(results) {
     const $reps = $('ul.reps');
     const result = await axios({
         method: 'post',
-        url: "https:localhost:3030/userData",
+        url: "http:localhost:3030/userData",
         withCredentials: true,
         data:{
             notes: " ",
